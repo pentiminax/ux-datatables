1b5e6d4ca8e2e1bf623e5a2e17f535bca747595e
Pentiminax <tanguy.lemarie.youtube@outlook.fr>
2025-02-06 21:18:02 +0100

    Update PHP dependencies

--- assets/src/controller.ts
+++ assets/src/controller.ts
@@ -1,12 +1,3 @@
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 import { Controller } from '@hotwired/stimulus';
 import DataTable from 'datatables.net-dt';
 
