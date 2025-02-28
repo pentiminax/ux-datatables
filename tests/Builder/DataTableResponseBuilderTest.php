afaa3d825c8d56066bfe0880c4a6d92d047d1420
Pentiminax <tanguy.lemarie.youtube@outlook.fr>
2025-02-28 10:09:11 +0100

    DataTableResponseBuilderTest

---
+++ tests/Builder/DataTableResponseBuilderTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Pentiminax\UX\DataTables\Tests\Builder;
+
+use Pentiminax\UX\DataTables\Builder\DataTableResponseBuilder;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use PHPUnit\Framework\TestCase;
+
+class DataTableResponseBuilderTest extends TestCase
+{
+    public function testBuildResponseWithDefaultValues(): void
+    {
+        $builder = new DataTableResponseBuilder();
+        $response = $builder->buildResponse();
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $data = json_decode($response->getContent(), true);
+
+        $this->assertEquals(1, $data['draw']);
+        $this->assertEquals(0, $data['recordsTotal']);
+        $this->assertEquals(0, $data['recordsFiltered']);
+        $this->assertEquals([], $data['data']);
+    }
+
+    public function testBuildResponseWithData(): void
+    {
+        $builder = new DataTableResponseBuilder();
+        $data = [
+            ['id' => 1, 'name' => 'Item 1'],
+            ['id' => 2, 'name' => 'Item 2']
+        ];
+        $response = $builder->buildResponse(2, $data);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $jsonData = json_decode($response->getContent(), true);
+
+        $this->assertEquals(2, $jsonData['draw']);
+        $this->assertEquals(2, $jsonData['recordsTotal']);
+        $this->assertEquals(2, $jsonData['recordsFiltered']);
+        $this->assertEquals($data, $jsonData['data']);
+    }
+
+    public function testBuildResponseWithCustomRecordCounts(): void
+    {
+        $builder = new DataTableResponseBuilder();
+        $data = [['id' => 1, 'name' => 'Item 1']];
+        $response = $builder->buildResponse(1, $data, 10, 5);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $jsonData = json_decode($response->getContent(), true);
+
+        $this->assertEquals(1, $jsonData['draw']);
+        $this->assertEquals(10, $jsonData['recordsTotal']);
+        $this->assertEquals(5, $jsonData['recordsFiltered']);
+        $this->assertEquals($data, $jsonData['data']);
+    }
+}
