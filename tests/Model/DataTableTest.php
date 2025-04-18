37390eea1ff2a7fbd2150e72f11cacbdfb87b520
Pentiminax <tanguy.lemarie.youtube@outlook.fr>
2025-04-18 12:17:32 +0200

    tests

--- tests/Model/DataTableTest.php
+++ tests/Model/DataTableTest.php
@@ -5,6 +5,7 @@ namespace Pentiminax\UX\DataTables\Tests\Model;
 use Pentiminax\UX\DataTables\Enum\Language;
 use Pentiminax\UX\DataTables\Model\DataTable;
 use Pentiminax\UX\DataTables\Model\Extensions\SelectExtension;
+use Pentiminax\UX\DataTables\Model\Options\AjaxOption;
 use Pentiminax\UX\DataTables\Model\Options\LayoutOption;
 use PHPUnit\Framework\TestCase;
 
@@ -17,6 +18,7 @@ class DataTableTest extends TestCase
         $table =
             (new DataTable('tableId'))
                 ->autoWidth(true)
+                ->ajax(new AjaxOption('/url'))
                 ->caption('Table caption')
                 ->deferRender(true)
                 ->displayStart(10)
