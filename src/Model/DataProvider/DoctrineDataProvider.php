c11524cf83a03c89516efb609cb8c971712d8010
Pentiminax <tanguy.lemarie.youtube@outlook.fr>
2025-10-03 18:25:47 +0200

    feat(chore): Add ability to customize query builder

--- src/Model/DataProvider/DoctrineDataProvider.php
+++ src/Model/DataProvider/DoctrineDataProvider.php
@@ -3,6 +3,7 @@
 namespace Pentiminax\UX\DataTables\Model\DataProvider;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
 use Pentiminax\UX\DataTables\Contracts\DataProviderInterface;
 use Pentiminax\UX\DataTables\Contracts\RowMapperInterface;
 use Pentiminax\UX\DataTables\Model\DataTableResult;
@@ -13,6 +14,8 @@ class DoctrineDataProvider implements DataProviderInterface
         private readonly EntityManagerInterface $em,
         private readonly string $entityClass,
         private readonly RowMapperInterface $rowMapper,
+        /** @var null|callable(QueryBuilder):QueryBuilder */
+        private $queryBuilderConfigurator = null
     ) {
     }
 
@@ -31,6 +34,11 @@ class DoctrineDataProvider implements DataProviderInterface
             ->createQueryBuilder()
             ->select($alias)
             ->from($this->entityClass, $alias);
+
+        if ($this->queryBuilderConfigurator) {
+            $qb = ($this->queryBuilderConfigurator)($qb);
+        }
+
 
         $filteredCountQb = clone $qb;
         $filteredCount = (int) $filteredCountQb
