907e5e855f7324d033560afaf59ec5bc94bbac30
Tanguy LEMARIE <tanguy.lemarie@skaleet.com>
2026-01-11 18:15:39 +0100

    chore(docs): Upgrade @astrojs/starlight to v0.32.0 and update site configuration

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
+  site: 'https://pentiminax.github.io',
+  base: '/ux-datatables',
   integrations: [
     starlight({
       title: 'UX DataTables',
