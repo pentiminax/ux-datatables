fa1a5b6b2abd3db9ec13d5f94038f2c644f03cd6
Tanguy LEMARIE <tanguy.lemarie@skaleet.com>
2026-02-11 18:20:37 +0100

    Fix

--- src/Controller/AjaxEditController.php
+++ src/Controller/AjaxEditController.php
@@ -2,7 +2,6 @@
 
 namespace Pentiminax\UX\DataTables\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Persistence\ObjectRepository;
 use Pentiminax\UX\DataTables\Dto\AjaxEditRequestDto;
@@ -14,38 +13,19 @@ use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
 final class AjaxEditController
 {
     public function __construct(
-        private readonly ?ManagerRegistry $doctrine = null,
+        private readonly ManagerRegistry $doctrine,
         private readonly PropertyAccessorInterface $propertyAccessor,
     ) {
     }
 
     public function __invoke(#[MapRequestPayload] AjaxEditRequestDto $payload): Response
     {
-        if (null === $this->doctrine) {
-            return $this->jsonError('Doctrine is required to update boolean values.', Response::HTTP_NOT_IMPLEMENTED);
-        }
-
         $entityClass = $payload->entity;
-
-        $manager = $this->doctrine->getManagerForClass($entityClass);
-        if (!$manager instanceof EntityManagerInterface) {
-            return $this->jsonError('Entity manager not found for provided entity.', Response::HTTP_BAD_REQUEST);
-        }
-
-        $metadata = $manager->getClassMetadata($entityClass);
-        $field    = $payload->field;
-
-        if (!$metadata->hasField($field)) {
-            return $this->jsonError(sprintf('Field "%s" does not exist on "%s".', $field, $entityClass), Response::HTTP_BAD_REQUEST);
-        }
-
-        $fieldType = strtolower((string) $metadata->getTypeOfField($field));
-        if (!\in_array($fieldType, ['bool', 'boolean'], true)) {
-            return $this->jsonError(sprintf('Field "%s" must be mapped as boolean.', $field), Response::HTTP_BAD_REQUEST);
-        }
+        $field       = $payload->field;
+        $manager     = $this->doctrine->getManagerForClass($entityClass);
 
         /** @var ObjectRepository<object> $repository */
-        $repository = $manager->getRepository($entityClass);
+        $repository = $manager?->getRepository($entityClass);
         $id         = $payload->id;
         $entity     = $repository->find($id);
 
@@ -56,10 +36,12 @@ final class AjaxEditController
         $newValue = $payload->newValue;
 
         if (!$this->updateProperty($entity, $field, $newValue)) {
-            return $this->jsonError(sprintf('Unable to write "%s" on the entity.', $field), Response::HTTP_BAD_REQUEST);
+            return $this->jsonError(
+                \sprintf('Unable to write "%s" on the entity.', $field), Response::HTTP_BAD_REQUEST
+            );
         }
 
-        $manager->flush();
+        $manager?->flush();
 
         return new Response($newValue ? '1' : '0');
     }
