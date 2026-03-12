48536c0a03a34941fe083238668f04735c8d2388
Tanguy LEMARIE <tanguy.lemarie@skaleet.com>
2026-03-12 22:12:05 +0100

    Upgrade dependencies: bump Astro to 6.0.4, @astrojs/mdx to 5.0.0, and @astrojs/sitemap to 3.7.1; update related package versions and add new dependencies.

--- docs/src/content.config.js
+++ docs/src/content.config.js
@@ -1,4 +1,5 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection } from 'astro:content'
+import { z } from 'astro/zod'
 import { glob } from 'astro/loaders'
 const docs = defineCollection({
   loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/docs' }),
